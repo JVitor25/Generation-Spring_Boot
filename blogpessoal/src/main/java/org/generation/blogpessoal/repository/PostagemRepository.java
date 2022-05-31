e4aef2c6f4b64fd3946cf76cf21ecb0751061f82
Joao Vitor <joaovitorsocial25@gmail.com>
2022-05-31 15:02:35 -0300

    Segunda tabela finalizada

--- blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -12,4 +12,5 @@ de postagens (tb_postagens)*/
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
 }
