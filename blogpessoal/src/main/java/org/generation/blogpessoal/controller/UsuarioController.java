52e5ea2af36397b16d981f589e4118aeae93a861
Joao Vitor <joaovitorsocial25@gmail.com>
2022-06-09 00:22:13 -0300

    GetAll e ByID na nova controller

--- blogpessoal/src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
@@ -1,7 +1,9 @@
 package org.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.generation.blogpessoal.model.Tema;
 import org.generation.blogpessoal.model.UserLogin;
 import org.generation.blogpessoal.model.Usuario;
 import org.generation.blogpessoal.service.UsuarioService;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +26,17 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll(){
+		return ResponseEntity.ok(usuarioService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id){
+		return usuarioService.findById(id).map(resp -> ResponseEntity.ok(resp))
+			.orElse(ResponseEntity.notFound().build());	
+	}
+	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
@@ -31,7 +46,7 @@ public class UsuarioController {
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){
 		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+				.body(usuarioService.cadastrarUsuario(usuario));
 	}
 	
 }
